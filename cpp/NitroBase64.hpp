5ed4097f152e05a30b552c7c51a04f0020faae07
Phirom <chanphirom.sok@onego.asia>
2025-09-30 09:42:03 +0700

    fix: perf under 1ms decode/encode

--- cpp/NitroBase64.hpp
+++ cpp/NitroBase64.hpp
@@ -2,29 +2,17 @@
 
 #include "HybridNitroBase64Spec.hpp"
 #include "simdutf.h"
-//#include <iostream>
-//#include <chrono>
 
 namespace margelo::nitro::nitrobase64 {
     using namespace margelo::nitro;
 
     class NitroBase64 : public HybridNitroBase64Spec {
     public:
-        NitroBase64() : HybridObject(TAG) {
-//            DEBUG
-//            const simdutf::implementation* impl = simdutf::get_active_implementation();
-//            std::cout << "[NitroBase64] Active implementation: " << impl->name() << std::endl;
-//            std::cout << "[NitroBase64] Description: " << impl->description() << std::endl;
-        }
+        NitroBase64() : HybridObject(TAG) {}
 
         std::string encode(const std::string &input) override {
-//            auto start = std::chrono::high_resolution_clock::now();
-            
             size_t base64_size = simdutf::base64_length_from_binary(input.size());
-            std::string output;
-            output.resize(base64_size);
-            
-//            auto t1 = std::chrono::high_resolution_clock::now();
+            std::string output(base64_size, '\0');
             
             size_t actual_size = simdutf::binary_to_base64(
                 input.data(),
@@ -32,32 +20,17 @@ namespace margelo::nitro::nitrobase64 {
                 output.data()
             );
             
-//            auto t2 = std::chrono::high_resolution_clock::now();
-            
             output.resize(actual_size);
-//            auto end = std::chrono::high_resolution_clock::now();
-//            
-//            auto total = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-//            auto encoding = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-//            
-//            std::cout << "[encode] Total: " << total << "μs, Encoding: " << encoding
-//                      << "μs, Size: " << input.size() << " bytes" << std::endl;
-            
             return output;
         }
 
         std::string decode(const std::string &base64) override {
-//            auto start = std::chrono::high_resolution_clock::now();
-            
             size_t max_decoded_size = simdutf::maximal_binary_length_from_base64(
                 base64.data(),
                 base64.size()
             );
             
-            std::string output;
-            output.resize(max_decoded_size);
-            
-//            auto t1 = std::chrono::high_resolution_clock::now();
+            std::string output(max_decoded_size, '\0');
             
             auto result = simdutf::base64_to_binary(
                 base64.data(),
@@ -65,24 +38,13 @@ namespace margelo::nitro::nitrobase64 {
                 output.data()
             );
             
-//            auto t2 = std::chrono::high_resolution_clock::now();
-            
             if (result.error != simdutf::error_code::SUCCESS) {
                 throw std::runtime_error("Base64 decode failed");
             }
             
             output.resize(result.count);
-//            auto end = std::chrono::high_resolution_clock::now();
-//            auto total = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-//            auto decoding = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-//            
-//            std::cout << "[decode] Total: " << total << "μs, Decoding: " << decoding
-//                      << "μs, Size: " << base64.size() << " bytes" << std::endl;
-            
             return output;
         }
-
-      
 
     private:
         static constexpr auto TAG = "NitroBase64";
